d12dd92fbea1cd3815a92a9a2d0212de734cbd3d
mayisnow <131052559+mayisnow@users.noreply.github.com>
2023-04-18 21:28:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+#streamlit.stop()
 
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
